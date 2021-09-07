1756e73e651f9c66800d5c116945d3255d305bd9
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-09-07 23:13:12 +0530

    Updated Visitor count logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,16 +189,12 @@ themeButton.addEventListener('click', () => {
 })
 
 /*==================== DARK LIGHT THEME ====================*/
-var counterContainer = document.querySelector(".visitor__count");
-var visitCount = localStorage.getItem("page_view");
-
-// Check if page_view entry is present
-if (visitCount) {
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view", visitCount);
-} 
-else {
-    visitCount = 1;
-    localStorage.setItem("page_view", 1);
+const counterContainer = document.getElementById('visitor-count');
+updateVisitorCount();
+function updateVisitorCount() {
+    fetch('https://api.countapi.xyz/update/rushabh/portfolio/?amount=1')
+    .then(res => res.json())
+    .then(res => {
+        counterContainer.innerHTML = res.value;
+    });
 }
-counterContainer.innerHTML = visitCount;
